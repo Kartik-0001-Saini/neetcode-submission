c73f637abefc6620891701794c220f34652d9275
Karitk <kartiksaini7988@gmail.com>
2026-09-15 00:21:18 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    static int x;
+    static bool compare(int a, int b){
+        if(abs(a-x) == abs(b-x)){
+            return a<b;
+        }
+
+        return abs(a-x) < abs(b-x);
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int tar) {
+        x = tar;
+        sort(arr.begin(), arr.end(), compare);
+
+        vector<int> res(arr.begin(), arr.begin()+k);
+        sort(res.begin(), res.end());
+
+        return res;
+    }
+};
+
+int Solution::x = 0;
